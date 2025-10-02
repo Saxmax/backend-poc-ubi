6e44b82238b6bff1ce3d31c1c5bff7b726a22ae8
Max Huber <max@maxhuber.se>
2025-10-02 10:34:36 +0200

    updated device types

--- src/interfaces/Device.ts
+++ src/interfaces/Device.ts
@@ -2,12 +2,13 @@ import * as z from 'zod';
 
 export const DeviceType = z.object({
   id: z.number().optional(),
+  mac_address: z.string(),
   status: z.number().optional(),
-  checksum: z.number().optional(),
+  checksum: z.string().optional(),
   url: z.string().optional(),
-  firmware: z.number().optional(),
-  hardware: z.number().optional(),
-  software: z.number().optional(),
+  firmware: z.string().optional(),
+  hardware: z.string().optional(),
+  software: z.string().optional(),
 });
 
 export type IDevice = z.infer<typeof DeviceType>;
